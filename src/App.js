4e1015568e4fc4c240c2ba176994b7cb1d398271
abhishekadvani <a9819191140@gmail.com>
2023-04-16 17:05:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {/* 1 of 5 containers */}
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Hats now</p>
+        </div>
+      </div>
+
+      {/* 2 of 5 containers */}
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Jackets now</p>
+        </div>
+      </div>
+
+      {/* 3 of 5 containers */}
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Sneakers now</p>
+        </div>
+      </div>
+
+      {/* 4 of 5 containers */}
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <p>Shop Womens clothing now</p>
+        </div>
+      </div>
+
+      {/* 5 of 5 containers */}
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop Mens clothing now</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
