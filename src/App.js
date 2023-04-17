4c266508c1f383f2f995ef4a3c89d5a46fdd08ce
abhishekadvani <a9819191140@gmail.com>
2023-04-17 09:05:37 +0530

    directory component and catgeory added

--- src/App.js
+++ src/App.js
@@ -1,47 +1,30 @@
+import Directory from "./components/directory/directory.components.jsx";
 const App = () => {
-  return (
-    <div className="categories-container">
-      {/* 1 of 5 containers */}
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Hats now</p>
-        </div>
-      </div>
+  const categories = [
+    { id: 1, title: "hats", imageUrl: "https://i.ibb.co/cvpntL1/hats.png" },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
 
-      {/* 2 of 5 containers */}
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Jackets now</p>
-        </div>
-      </div>
-
-      {/* 3 of 5 containers */}
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Sneakers now</p>
-        </div>
-      </div>
-
-      {/* 4 of 5 containers */}
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Womens clothing now</p>
-        </div>
-      </div>
-
-      {/* 5 of 5 containers */}
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Mens clothing now</p>
-        </div>
-      </div>
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
